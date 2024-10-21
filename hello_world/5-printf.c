0d7124381708e7c07cd78a41f76e4eb0e4d0cbd8
SoliraZ <9937@holbertonstudents.com>
2024-10-21 10:38:54 +0200

    created 5-printf.c

---
+++ hello_world/5-printf.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * ft_printf - Fonction who write the line.
+ */
+
+void	ft_printf(void)
+{
+	printf("%s", "with proper grammar, but the outcome is a piece of art,");
+}
+
+/**
+ * main - Main fonction.
+ * Return: 0
+ */
+
+int	main(void)
+{
+	ft_printf();
+	return (0);
+}
