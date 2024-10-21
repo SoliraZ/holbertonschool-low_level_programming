ade72ea37ef7884725138aae0652234d7d171fef
SoliraZ <9937@holbertonstudents.com>
2024-10-21 11:50:50 +0200

    created 6-size.c

---
+++ hello_world/6-size.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+/**
+ * ft_size - Write the all size of any variable types.
+ */
+
+void	ft_size(void)
+{
+	char	charType;
+	int	intType;
+	unsigned int	longintType;
+	double	longlongintType;
+	float	floatType;
+
+	printf("Size of a char: %zu byte(s)\n", sizeof(charType));
+	printf("Size of an int: %zu byte(s)\n", sizeof(intType));
+	printf("Size of a long int: %zu byte(s)\n", sizeof(longintType));
+	printf("Size of a long long int: %zu byte(s)\n", sizeof(longlongintType));
+	printf("Size of a float: %zu byte(s)\n", sizeof(floatType));
+}
+
+/**
+ * main - Main fonction.
+ * Return: 0
+ */
+
+int	main(void)
+{
+	ft_size();
+	return (0);
+}
