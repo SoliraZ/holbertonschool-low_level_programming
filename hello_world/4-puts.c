015d15ac48fe8f9eb772d2184ad4a98aed286dbc
SoliraZ <9937@holbertonstudents.com>
2024-10-21 10:26:39 +0200

    created 4-puts.c

---
+++ hello_world/4-puts.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * ft_puts - Fonction who write the line.
+ * Return: 0
+ */
+
+void	ft_puts(void)
+{
+	puts("\"Programming is like building a multilingual puzzle\n");
+}
+
+/**
+ * main - Main of the project
+ * Description: Write the phrase that we have to print.
+ * Return: 0
+ */
+
+int	main(void)
+{
+	ft_puts();
+	return (0);
+}
