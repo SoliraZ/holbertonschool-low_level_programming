5ef574d4ae95aa173945b051f5dcfc58c2af225f
SoliraZ <9937@holbertonstudents.com>
2024-10-22 22:52:09 +0200

    created 4-isalpha.c

---
+++ functions_nested_loops/4-isalpha.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * _islower - Say if its lower case or no.
+ *
+ * @c: C is char.
+ * Return: 1 if its lowercase else return 0
+ */
+
+int	_islower(int c)
+{
+	if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
+		return (1);
+	return (0);
+}
