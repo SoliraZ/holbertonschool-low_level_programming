3d633d613e6182e1fe18a36f0d538d2440a3dacf
SoliraZ <9937@holbertonstudents.com>
2024-10-22 21:36:25 +0200

    fixed 2-print_alphabet_x10.c

--- functions_nested_loops/2-print_alphabet_x10.c
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -4,11 +4,11 @@
 #include "main.h"
 
 /**
- * print_alphabet - Print alphabet
+ * print_alphabet_x10 - Print alphabet x10
  *
  */
 
-void	print_alphabet(void)
+void	print_alphabet_x10(void)
 {
 	int	n;
 
