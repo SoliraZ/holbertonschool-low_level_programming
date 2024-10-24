8cab9f94e3cbd22eb82752eda6555a6b18b67836
SoliraZ <9937@holbertonstudents.com>
2024-10-24 12:37:24 +0200

    created 10-add.c

---
+++ functions_nested_loops/10-add.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * add - Additionate two variable.
+ * 
+ * Return: 0
+ */
+
+int	add(int i, int j)
+{
+	return (i + j);
+}
---
+++ functions_nested_loops/9-times_table.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <unistd.h>
+#include "main.h"
+
+/**
+ * times_table - Table of nine.
+ */
+
+void	times_table(void)
+{
+	int	i;
+
+	i = 0;
+	while (i <= 9)
+	{
+		_putchar()
+	}
+}
