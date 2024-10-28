c039ec96ed93ed42723a5ebbc377e240f1ef2105
SoliraZ <9937@holbertonstudents.com>
2024-10-28 18:56:44 +0100

    main.h

---
+++ pointers_arrays_strings/main.h
@@ -0,0 +1,17 @@
+#ifndef _PUTCHAR
+#define _PUTCHAR
+
+void	_putchar();
+void	reset_to_98();
+void	swap_int();
+int	_strlen();
+void	_puts();
+void	print_rev();
+void	rev_string();
+void	puts2();
+void	puts_half();
+void	print_array();
+char	*_strcpy();
+int	_atoi();
+
+#endif
