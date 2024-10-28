d81f70cefb88edfcceb60074120d8505ba64ff96
SoliraZ <9937@holbertonstudents.com>
2024-10-28 22:09:22 +0100

    created 7-puts_half.c

---
+++ pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * puts_half - Print Half of string
+ * @str: String
+ */
+
+void	puts_half(char *str)
+{
+	int	i;
+
+	i = 0;
+	while (str[i] != '\0')
+		i++;
+	i = i / 2;
+	while (str[i] != '\0')
+	{
+		_putchar(str[i]);
+		i++;
+	}
+	_putchar('\n');
+}
