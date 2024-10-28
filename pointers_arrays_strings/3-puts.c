1c123accdbf8394c52bedd307fc948d7a0aea1d0
SoliraZ <9937@holbertonstudents.com>
2024-10-28 19:17:48 +0100

    fixed 3-puts.c and edited to use write

--- pointers_arrays_strings/3-puts.c
+++ pointers_arrays_strings/3-puts.c
@@ -1,5 +1,5 @@
 #include "main.h"
-#include <stdio.h>
+#include <unistd.h>
 
 /**
  * _puts - Function _puts
@@ -8,5 +8,13 @@
 
 void	_puts(char *str)
 {
-	printf("%s\n", str);
+	int	i;
+
+	i = 0;
+	while (str[i] != '\0')
+	{
+		write(1, &str[i], 1);
+		i++;
+	}
+	write (1, "\n", 1);
 }
