17d416b4a28df5b145f8c4cd732e3cdd34ca0b5e
SoliraZ <9937@holbertonstudents.com>
2024-10-28 08:49:37 +0100

    created main.h

---
+++ main.h
@@ -0,0 +1,15 @@
+#ifndef _PUTCHAR
+#define _PUTCHAR
+
+int	_isupper();
+int	_isdigit();
+int	mul();
+void	print_numbers();
+void	print_most_numbers();
+void	more_numbers();
+void	print_line();
+void	print_diagonal();
+void	print_square();
+void	print_triangle();
+
+#endif
