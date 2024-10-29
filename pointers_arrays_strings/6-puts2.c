f194de90932ba4210bc033872537126fb298feca
SoliraZ <9937@holbertonstudents.com>
2024-10-29 09:42:19 +0100

    6-puts2 fixed

--- pointers_arrays_strings/6-puts2.c
+++ pointers_arrays_strings/6-puts2.c
@@ -8,9 +8,13 @@
 void	puts2(char *str)
 {
 	int	i;
+	int	l;
 
 	i = 0;
-	while (str[i] != '\0')
+	l = 0;
+	while (str[l] != '\0')
+		l++;
+	while (str[i] != '\0' && i <= l)
 	{
 		_putchar(str[i]);
 		i = i + 2;
