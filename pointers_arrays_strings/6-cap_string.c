f7f4c34c7e0b508c6129355393d6df1bde17ba66
SoliraZ <9937@holbertonstudents.com>
2024-10-30 20:21:15 +0100

    fixed 6-cap_string.c

--- pointers_arrays_strings/6-cap_string.c
+++ pointers_arrays_strings/6-cap_string.c
@@ -12,10 +12,10 @@ char	*cap_string(char *str)
 	int	i;
 
 	i = 0;
+	if (str[0] >= 'a' && str[0] <= 'z')
+		str[0] = str[0] - 32;
 	while (str[i])
 	{
-		if (str[0] >= 'a' && str[0] <= 'z')
-			str[0] = str[0] - 32;
 		if ((str[i] >= 'a' && str[i] <= 'z') && (str[i - 1] == 44 || str[i - 1] == 32 || str[i - 1] == 59 || str[i - 1] == 46 || str[i - 1] == 33 || str[i - 1] == 63 || str[i - 1] == 34 || str[i - 1] == 40 || str[i - 1] == 41 || str[i - 1] == 123 || str[i - 1] == 125))
 			str[i] = str[i] - 32;
 		i++;
