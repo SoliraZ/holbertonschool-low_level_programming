4ecc92b3f93c0f456a4be8f6e0dcc9b08676b57f
SoliraZ <9937@holbertonstudents.com>
2024-10-30 18:49:41 +0100

    created 1-strncat.c

---
+++ pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * _strncat - Function Strcat
+ * @dest: Destination
+ * @src: Source
+ * @n: Size
+ *
+ * Return: string
+ */
+
+char	*_strncat(char *dest, char *src, int n)
+{
+	int	i;
+	int	l;
+
+	i = 0;
+	l = 0;
+	while (dest[i])
+	{
+		i++;
+	}
+	while (src[l] != '\0' && src[l] < n)
+	{
+		dest[i] = src[l];
+		i++;
+		l++;
+	}
+	dest[i] = '\0';
+	return (dest);
+}
