85eec2501518bb3af7bfb0c5fa452275533e5eed
SoliraZ <9937@holbertonstudents.com>
2024-10-30 14:01:21 +0100

    main.h (added function of project more pointers)

--- pointers_arrays_strings/main.h
+++ pointers_arrays_strings/main.h
@@ -14,4 +14,13 @@ void	print_array();
 char	*_strcpy();
 int	_atoi();
 
+char	*_strcat();
+char	*_strncat();
+char	*_strncpy();
+int	_strcmp();
+void	reverse_array();
+char	*string_toupper();
+char	*cap_string();
+char	*leet();
+
 #endif
