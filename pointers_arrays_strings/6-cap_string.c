9673b5e401db52075f1cc89d7d95175abc52358f
SoliraZ <9937@holbertonstudents.com>
2024-10-30 20:50:15 +0100

    fixed 6-cap_string.c

--- pointers_arrays_strings/6-cap_string.c
+++ pointers_arrays_strings/6-cap_string.c
@@ -16,7 +16,12 @@ char	*cap_string(char *str)
 		str[0] = str[0] - 32;
 	while (str[i])
 	{
-		if ((str[i] >= 'a' && str[i] <= 'z') && (str[i - 1] == 44 || str[i - 1] == 32 || str[i - 1] == 59 || str[i - 1] == 46 || str[i - 1] == 33 || str[i - 1] == 63 || str[i - 1] == 34 || str[i - 1] == 40 || str[i - 1] == 41 || str[i - 1] == 123 || str[i - 1] == 125))
+		if ((str[i] >= 'a' && str[i] <= 'z') &&
+			(str[i - 1] == ' ' || str[i - 1] == '\t' || str[i - 1] == '\n' ||
+			str[i - 1] == ',' || str[i - 1] == ';' || str[i - 1] == '.' ||
+			str[i - 1] == '!' || str[i - 1] == '?' || str[i - 1] == '"' ||
+			str[i - 1] == '(' || str[i - 1] == ')' || str[i - 1] == '{' ||
+			str[i - 1] == '}'))
 			str[i] = str[i] - 32;
 		i++;
 	}
