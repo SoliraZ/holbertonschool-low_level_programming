f7a8028382682e804222ae24c86b4fba4aca1ff5
SoliraZ <9937@holbertonstudents.com>
2024-11-05 18:39:19 +0100

    created 1-print_rev_recursion.c

---
+++ recursion/1-print_rev_recursion.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _print_rev_recursion - Print in reverse recursive Function
+ * @s: char *
+ */
+
+#include <stdio.h>
+
+void    _print_rev_recursion(char *s)
+{
+	if (*s == '\0')
+	{
+		return;
+	}
+	_print_rev_recursion(s + 1);
+	putchar(*s);
+}
