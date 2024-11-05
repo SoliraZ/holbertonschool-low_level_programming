2999264c74630d49375c5623ace364cc4f5ab828
SoliraZ <9937@holbertonstudents.com>
2024-11-05 14:09:39 +0100

    created 4-strpbrk.c

---
+++ pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,29 @@
+#include "main.h"
+#include <stddef.h>
+/**
+ * _strpbrk - STRPBRK Function
+ * @s: char *
+ * @accept: char *
+ *
+ * Return: Char
+ */
+
+char	*_strpbrk(char *s, char *accept)
+{
+	int	n;
+
+	if (s[0] == '\0' || accept[0] == '\0')
+		return ("test1");
+	while (*s != '\0')
+	{
+		n = 0;
+		while (accept[n])
+		{
+			if (*s == accept[n])
+				return (s);
+			n++;
+		}
+		s++;
+	}
+	return ("test2");
+}
