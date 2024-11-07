697efbd339b4844680943a1f6d41031fddf4b8a2
SoliraZ <9937@holbertonstudents.com>
2024-11-07 08:54:31 +0100

    ?

--- recursion/6-is_prime_number.c
+++ recursion/6-is_prime_number.c
@@ -1,20 +1,6 @@
 #include "main.h"
 
 /**
- * prime_number - Function Prime Number
- * @n: Int
- *
- * Return: 1 if yes, 0 if no.
- */
-
-int	prime_number(int n)
-{
-	if (n < 2)
-		return (0);
-	return (1);
-}
-
-/**
  * is_prime_number - Function Prime Number
  * @n: Int
  *
@@ -35,5 +21,4 @@ int	is_prime_number(int n)
 		i++;
 	}
 	return (1);
-	prime_number(n);
 }
