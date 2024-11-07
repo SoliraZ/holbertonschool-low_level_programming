bc15ab344e610773a8bf063603b0a49e10a7816a
SoliraZ <9937@holbertonstudents.com>
2024-11-07 20:58:29 +0100

    created 4-add.c

---
+++ argc_argv/4-add.c
@@ -0,0 +1,48 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+
+/**
+ * main - Main Function
+ * @argc: Int
+ * @argv: Char
+ *
+ * Return: 0 if all work, 1 if there is an error
+ */
+
+int	main(int argc, char *argv[])
+{
+	int	i;
+	int	j;
+	int	res;
+
+	i = 1;
+	res = 0;
+	if (argc == 1)
+	{
+		printf("0\n");
+		return (1);
+	}
+	if (argc == 2)
+	{
+		printf("%i\n", atoi(argv[1]));
+		return (0);
+	}
+	while (i < argc)
+	{
+		j = 0;
+		while (argv[i][j])
+		{
+			if (!isdigit(argv[i][j]))
+			{
+				printf("Error\n");
+				return (1);
+			}
+			j++;
+		}
+		res += atoi(argv[i]);
+		i++;
+	}
+	printf("%i\n", res);
+	return (0);
+}
