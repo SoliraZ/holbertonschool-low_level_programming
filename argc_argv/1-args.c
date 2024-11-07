8db12d8cf801465c3794cadac79f4ad0c5758a1e
SoliraZ <9937@holbertonstudents.com>
2024-11-07 18:54:00 +0100

    fixed 1-args.c

--- argc_argv/1-args.c
+++ argc_argv/1-args.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <stdio.h>
 
 /**
  * main - Main Function
@@ -12,5 +13,6 @@ int	main(int argc, char *argv[])
 {
 	argc--;
 	(void)argv;
-	return (argc);
+	printf("%i\n", argc);
+	return (0);
 }
