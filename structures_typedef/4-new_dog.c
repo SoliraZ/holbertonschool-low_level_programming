e91b6b9cb5f3b34135ae73f735ed8898108a34c6
SoliraZ <48773711+SoliraZ@users.noreply.github.com>
2024-11-15 23:28:34 +0100

    i have headache

--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -13,16 +13,24 @@
 
 dog_t	*new_dog(char *name, float age, char *owner)
 {
-	dog_t	*new_dog;
+	dog_t *new_dog;
 
-	new_dog = malloc(sizeof(dog_t));
-	if (new_dog == NULL)
+	if (!name || !owner)
 		return (NULL);
-
-	if (name == NULL)
-		name = "(nil)";
-	new_dog->name = name;
+	new_dog = malloc(sizeof(dog_t));
+	if (!new_dog)
+		return (NULL);
+	new_dog->name = malloc(strlen(name) + 1);
+	new_dog->owner = malloc(strlen(owner) + 1);
+	if (!new_dog->name || !new_dog->owner)
+	{
+		free(new_dog->name);
+		free(new_dog->owner);
+		free(new_dog);
+		return (NULL);
+	}
+	strcpy(new_dog->name, name);
+	strcpy(new_dog->owner, owner);
 	new_dog->age = age;
-	new_dog->owner = owner;
 	return (new_dog);
 }
