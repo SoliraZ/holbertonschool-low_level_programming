59f3c5522f9d89c69136e28bf0003f189616822a
SoliraZ <48773711+SoliraZ@users.noreply.github.com>
2024-11-15 22:00:11 +0100

    fixed 4-new_dog.c

--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -18,6 +18,10 @@ dog_t	*new_dog(char *name, float age, char *owner)
 	if (new_dog == NULL)
 		return (NULL);
 
+	if (name == NULL)
+		name = "(nil)";
+	if (age == NULL)
+		age = "(nil)";
 	new_dog->name = name;
 	new_dog->age = age;
 	new_dog->owner = owner;
