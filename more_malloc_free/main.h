4ebd8e7238bec7cf75196d71c354ba542a109bde
SoliraZ <48773711+SoliraZ@users.noreply.github.com>
2024-11-15 18:05:40 +0100

    edited main.h and 0-malloc_checked.c

--- more_malloc_free/main.h
+++ more_malloc_free/main.h
@@ -1,9 +1,9 @@
-#ifndef MORE_MALLOC_FREE_MAIN_H
-#define MORE_MALLOC_FREE_MAIN_H
+#ifndef MORE_MALLOC_FREE
+#define MORE_MALLOC_FREE
 
-void *malloc_checked(unsigned int b);
-char *string_nconcat(char *s1, char *s2, unsigned int n);
-void *_calloc(unsigned int nmemb, unsigned int size);
-int *array_range(int min, int max); 
+void	*malloc_checked(unsigned int b);
+char	*string_nconcat(char *s1, char *s2, unsigned int n);
+void	*_calloc(unsigned int nmemb, unsigned int size);
+int	*array_range(int min, int max); 
 
 #endif
