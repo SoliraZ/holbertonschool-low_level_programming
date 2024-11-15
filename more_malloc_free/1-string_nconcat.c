5d31a4683521262fa66e3f914e68621f34a96ad0
SoliraZ <48773711+SoliraZ@users.noreply.github.com>
2024-11-15 19:29:59 +0100

    fixed 1-string_nconcat.c

--- more_malloc_free/1-string_nconcat.c
+++ more_malloc_free/1-string_nconcat.c
@@ -14,6 +14,10 @@ char	*string_nconcat(char *s1, char *s2, unsigned int n)
 	char	*str;
 	unsigned int	i = 0, j = 0;
 
+	if (s1 == NULL)
+		s1 = "";
+	if (s2 == NULL)
+		s2 = "";
 	while (s1[i])
 		i++;
 	while (s2[j])
@@ -21,7 +25,7 @@ char	*string_nconcat(char *s1, char *s2, unsigned int n)
 	if (n >= j)
 		n = j;
 	str = malloc(sizeof(char) * (i + n + 1));
-	if (!str)
+	if (str == NULL)
 		return (NULL);
 	j = 0;
 	while (j < i)
