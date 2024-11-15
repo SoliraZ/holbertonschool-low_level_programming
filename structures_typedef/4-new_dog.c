1941281f3157cceb43fd27cf8c8c04269b9ea0ad
SoliraZ <48773711+SoliraZ@users.noreply.github.com>
2024-11-15 23:38:27 +0100

    fixed 4-new_dog.c

--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -13,24 +13,16 @@
 
 dog_t	*new_dog(char *name, float age, char *owner)
 {
-	dog_t *new_dog;
+	dog_t	*new_dog;
 
-	if (!name || !owner)
-		return (NULL);
 	new_dog = malloc(sizeof(dog_t));
-	if (!new_dog)
+	if (new_dog == NULL)
 		return (NULL);
-	new_dog->name = malloc(strlen(name) + 1);
-	new_dog->owner = malloc(strlen(owner) + 1);
-	if (!new_dog->name || !new_dog->owner)
-	{
-		free(new_dog->name);
-		free(new_dog->owner);
-		free(new_dog);
-		return (NULL);
-	}
-	strcpy(new_dog->name, name);
-	strcpy(new_dog->owner, owner);
+
+	if (name == NULL)
+		name = "(nil)";
+	new_dog->name = name;
 	new_dog->age = age;
+	new_dog->owner = owner;
 	return (new_dog);
 }
