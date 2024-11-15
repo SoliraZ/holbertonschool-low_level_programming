e3c774439745f54fd42ae3cae066e46b14c50675
SoliraZ <48773711+SoliraZ@users.noreply.github.com>
2024-11-15 18:22:50 +0100

    modified main.h

--- more_malloc_free/main.h
+++ more_malloc_free/main.h
@@ -2,11 +2,6 @@
 #define MORE_MALLOC_FREE
 
 int _putchar(char c);
-char *create_array(unsigned int size, char c);
-char *_strdup(char *str);
-char *str_concat(char *s1, char *s2);
-int **alloc_grid(int width, int height);
-void free_grid(int **grid, int height);
 void *malloc_checked(unsigned int b);
 char *string_nconcat(char *s1, char *s2, unsigned int n);
 void *_calloc(unsigned int nmemb, unsigned int size);
