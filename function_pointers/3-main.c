6f9a74e01bb47e3c922c4265a87090f6203b6094
SoliraZ <48773711+SoliraZ@users.noreply.github.com>
2024-11-20 22:17:49 +0100

    created 3-main.c/3-get_op_func.c/3-op_functions.c and 3-calc.h header

---
+++ function_pointers/3-main.c
@@ -0,0 +1,32 @@
+#include "3-calc.h"
+
+/**
+ * main - Entry point
+ * @argc: The number of command line arguments
+ * @argv: An array containing the program command line arguments
+ *
+ * Return: 0 (Success)
+ */
+
+int	main(int argc, char *argv[])
+{
+	if (argc < 4 || argc > 4)
+	{
+		printf("Error\n");
+		exit(98);
+	}
+	if (argv[2][0] != '/' && argv[2][0] != '+'
+			&& argv[2][0] != '-' && argv[2][0] != '*' && argv[2][0] != '%')
+	{
+		printf("Error\n");
+		exit(99);
+	}
+	if ((argv[2][0] == '/' || argv[2][0] == '%')
+			&& (argv[1][0] == '0' || argv[3][0] == '0'))
+	{
+		printf("Error\n");
+		exit(100);
+	}
+	printf("%d\n", get_op_func(argv[2])(atoi(argv[1]), atoi(argv[3])));
+	return (0);
+}
