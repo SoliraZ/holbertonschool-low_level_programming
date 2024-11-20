30034a89c8c838cebbb664629d64855d5c54044e
SoliraZ <48773711+SoliraZ@users.noreply.github.com>
2024-11-20 19:53:25 +0100

    fixed 2-int_index.c

--- function_pointers/2-int_index.c
+++ function_pointers/2-int_index.c
@@ -18,11 +18,14 @@ int	int_index(int *array, int size, int (*cmp)(int))
 	i = 0;
 	if (size <= 0)
 		return (-1);
-	while (i < size)
+	if (array && cmp)
 	{
-		if (cmp(array[i]) == 1)
-			return (i);
-		i++;
+		while (i < size)
+		{
+			if (cmp(array[i]) == 1)
+				return (i);
+			i++;
+		}
 	}
 	return (-1);
 }
