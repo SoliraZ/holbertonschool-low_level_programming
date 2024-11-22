d234193e4ea7d641c15a852ae7436ac3ac06a84a
SoliraZ <48773711+SoliraZ@users.noreply.github.com>
2024-11-22 19:57:56 +0100

    fixed 1-print_numbers.c

--- variadic_functions/1-print_numbers.c
+++ variadic_functions/1-print_numbers.c
@@ -13,19 +13,14 @@ void	print_numbers(const char *separator, const unsigned int n, ...)
 {
 	va_list args;
 	unsigned int	i;
-	int	j;
 
 	i = 0;
 	va_start(args, n);
 	while (i < n)
 	{
 		printf("%i", va_arg(args, int));
-		j = 0;
-		while (separator[j] && i < (n - 1))
-		{
-			printf("%c", separator[j]);
-			j++;
-		}
+		if (separator && i < (n - 1))
+			printf("%s", separator);
 		i++;
 	}
 	va_end(args);
