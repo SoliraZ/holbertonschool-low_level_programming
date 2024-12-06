ff7250bde31ccb8fccf1f3708036293bcb3ed8d2
SoliraZ <48773711+SoliraZ@users.noreply.github.com>
2024-12-06 13:28:43 +0100

    created 6-sum_dlistint.c

---
+++ doubly_linked_lists/6-sum_dlistint.c
@@ -0,0 +1,23 @@
+#include "lists.h"
+
+/**
+ * sum_dlistint - returns the sum of all the data (n) of a dlistint_t list.
+ * @head: pointer to the head of the list
+ *
+ * Return: the sum of all the data (n) of the list
+ */
+
+int	sum_dlistint(dlistint_t *head)
+{
+	dlistint_t *current = head;
+	int	result = 0;
+	
+	if (head == NULL)
+		return (0);
+	while (current)
+	{
+		result = result + current->n;
+		current = current->next;
+	}
+	return (result);
+}
